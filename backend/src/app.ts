f4c108fb904263e2d093e8b2347260ac064c41b9
Hung <viethung130905@gmail.com>
2026-01-30 17:08:58 +0700

    feat(backend): add error handler

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { errorHandler } from "./middlewares/errorHandler";
 export const app = express();
 
 app.use(cors({
@@ -10,3 +11,5 @@ app.use(cors({
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(errorHandler);
---
+++ backend/src/common/errors/ForbiddenError.ts
@@ -0,0 +1,7 @@
+import { HttpError } from "./HttpError";
+
+export class ForbiddenError extends HttpError {
+    constructor(message = "Forbidden") {
+        super(message, 403);
+    }
+}
